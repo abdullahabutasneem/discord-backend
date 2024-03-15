bd60a1a8571d2e289277fd6322c03de7ca829ea6
Abdullah Abu Tasneem <abdullahabutasneem19@gmail.com>
2024-03-16 02:23:18 +0600

    Created login functionality

--- controllers/auth/postLogin.js
+++ controllers/auth/postLogin.js
@@ -1,5 +1,25 @@
+const User = require('../../models/user');
+const bcrypt = require('bcryptjs');
+
 const postLogin = async (req, res) => {
-    res.send('login route');
+    try {
+        const { mail, password } = req.body;
+
+        const user = await User.findOne({ mail: mail.toLowerCase() });
+
+        if(user && (await bcrypt.compare(password, user.password))) {
+            return res.status(200).json({
+                userDetails: {
+                    mail: user.mail,
+                    username: user.username,
+                }
+            });
+        }
+
+        return res.status(400).send("Invalid credential. Please try again");
+    } catch (err) {
+        return res.status(500).send("Somehing went wrong. Please try again");
+    }
 }
 
 module.exports = postLogin;
