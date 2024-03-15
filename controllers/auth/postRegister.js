714f93ea53cb92cf0d7f2ee4b98b8b93e88862f5
Abdullah Abu Tasneem <abdullahabutasneem19@gmail.com>
2024-03-16 01:33:49 +0600

    Created register functionality: \n-Check duplicate email \n-Password encryption with bcryptjs. \n-Create user document and save it in database. \n-Sent response to client \n-Handling errors

--- controllers/auth/postRegister.js
+++ controllers/auth/postRegister.js
@@ -1,5 +1,36 @@
+const User = require('../../models/user');
+const bcrypt = require('bcryptjs');
+
 const postRegister = async (req, res) => {
-    res.send('register route');
+    try {
+        const { username, mail, password } = req.body;
+
+        // check if user exists
+        const userExists = await User.exists({ mail: mail.toLowerCase() });
+        if(userExists) {
+            return res.status(409).send("E-mail already in use.");
+        }
+
+        // encrypt password
+        const encryptedPassword = await bcrypt.hash(password, 10);
+
+        // create user document and save in database
+        const user = await User.create({
+            username,
+            mail: mail.toLowerCase(),
+            password: encryptedPassword,
+        });
+
+        // sending response to client
+        res.status(201).json({
+            userDetails: {
+                mail: uawe.mail,
+                username: user.username,
+            }
+        })
+    } catch {
+        return res.status(500).send("Error occured. Please try again");
+    }
 }
 
 module.exports = postRegister;
